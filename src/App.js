8d829fa44e39bbce683600a4a936eb394369d733
yashviradia <63194797+yashviradia@users.noreply.github.com>
2023-04-18 01:35:52 +0200

    debugged code once more

--- src/App.js
+++ src/App.js
@@ -102,7 +102,7 @@ class App extends Component {
   }
 
   onRouteChange = (route) => {
-    if (route === 'signin') {
+    if (route === 'signout') {
       this.setState({isSignedIn: false});
     } else if (route === 'home') {
       this.setState({isSignedIn: true});

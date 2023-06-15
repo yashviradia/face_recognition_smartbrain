a853bd1c0efc660bf5d4875e191f229367c4d522
yashviradia <y.viradia@tu-braunschweig.de>
2023-06-15 10:42:58 +0200

    implemented clarifai api once more, but still error

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './FaceRecognition.css';
 
-export default function FaceRecognition({imageUrl, box}) {
+export default function FaceRecognition({imageUrl}) {
   return (
     <div className='center ma'>
         <div className='absolute ma2'>
           <img id="inputimage" alt="" src={imageUrl} width="500px" height="auto"/>
-          <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+          {/* <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div> */}
         </div>
     </div> 
   );
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -18,7 +18,7 @@ const defaultOptions = {
 
 export default function Logo() {
   return (
-    <div className='ma4 mt0'>
+    <div className="ma4 mt0">
         <Tilt className="Tilt br2 shadow-2" options={defaultOptions} style={{ height: 150, width: 150 }}>        
             <div className="Tilt-inner pa3">
                 <img style={{paddingTop: '5px'}} alt='logo' src={brain}/>
